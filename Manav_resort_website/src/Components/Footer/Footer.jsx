059d1424d247afb6b1bc3872a0077563b64ee923
Manav Singla <70840284+Manu07-ux@users.noreply.github.com>
2022-11-23 03:16:29 +0530

    Add files via upload

---
+++ Manav_resort_website/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <h2>Beach Resort | Hotel Room Book</h2>
+      
+    </footer>
+  );
+}
